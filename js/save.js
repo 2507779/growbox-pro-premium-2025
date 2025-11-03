08ab4ff4371b6bff2cc4fb70e979fc2ba708c405
2507779 <2507779@gmail.com>
2025-11-03 21:50:12 +0300

    Update save and load functions for new save key

--- js/save.js
+++ js/save.js
@@ -1,24 +1,21 @@
 import { gameState } from './game.js';
 
 export function saveGame() {
-  localStorage.setItem('growbox_save_2025', JSON.stringify(gameState));
+  localStorage.setItem('growbox_save_2025_ultra', JSON.stringify(gameState));
 }
 
 export function loadGame() {
-  const data = localStorage.getItem('growbox_save_2025');
+  const data = localStorage.getItem('growbox_save_2025_ultra');
   if (data) {
     try {
       const saved = JSON.parse(data);
       Object.assign(gameState, saved);
     } catch (e) {
-      console.error('Ошибка загрузки');
+      console.error('Ошибка загрузки сохранения');
     }
   }
 }
 
-// Загрузка при старте
 loadGame();
-
-// Автосохранение
 setInterval(saveGame, 120000);
 window.addEventListener('beforeunload', saveGame);
